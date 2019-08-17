9e3c0c6f9f1e7c1033601ccfedfe0d0ed25cc96d
Elco Garcia <garciaelco@gmail.com>
2019-08-17 18:58:32 -0500

    minor changes

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var ticketsCtrl = require('../controllers/tickets');
 
 router.get('/:id/new/', ticketsCtrl.new);
+router.post('/:id', ticketsCtrl.create);
 
 module.exports = router;
