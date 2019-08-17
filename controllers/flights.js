17b1ef4ab1e48a4d7d5210556d3648f7efdbd4ed
Elco Garcia <garciaelco@gmail.com>
2019-08-17 11:57:48 -0500

    fixed page that shows details for the flight

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,8 +3,16 @@ var Flights = require('../models/flight');
 module.exports = {
 	index,
 	new: newFlight,
-	create
+	create,
+	show
 };
+
+function show(req, res) {
+	Flights.findById(req.params.id, function(err, flight) {
+		console.log(req.params.id);
+		res.render('flights/show', { flight });
+	});
+}
 function index(req, res) {
 	Flights.find({}, function(err, flights) {
 		res.render('flights/index', { flights });
